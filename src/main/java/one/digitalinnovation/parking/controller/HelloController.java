e7591ab7de2bce189af2e729b98b8cf426259f37
RomasCosta <romariocosta25@outlook.com>
2022-10-14 11:56:45 -0300

    Estrutura inicial

---
+++ src/main/java/one/digitalinnovation/parking/controller/HelloController.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package one.digitalinnovation.parking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author romar
+ */
+@RestController
+@RequestMapping("/")
+public class HelloController {
+    
+    @GetMapping
+    public String hello() {
+        return "Hello World...";
+    }
+}
